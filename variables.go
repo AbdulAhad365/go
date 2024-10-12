9768d6bc053aeaf6c4ae5cc133f7ebd07edffeb5
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-12 19:55:01 +0600

    how to use or store vaibales in go

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+	//assign
+	var name string="golang"
+	fmt.Println(name)
+
+	var adult=true
+	fmt.Println(adult)
+
+	//short syntax
+	roll_no:=004
+	fmt.Println(roll_no)
+}
