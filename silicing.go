93909508fcc820bb1beb9af118f45eb29e4f3d52
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-19 00:06:40 +0600

    silicing update

--- silicing.go
+++ silicing.go
@@ -2,9 +2,9 @@ package main
 import "fmt"
 func main(){
 	var nums[]int
-	fmt.Println(nums)
+	// fmt.Println(nums)
 	fmt.Println(nums==nil)
-	fmt.Println(len(nums))
+	// fmt.Println(len(nums))
 
 	// part 2
 	// it resize the capticty by double when more space is req
@@ -15,18 +15,27 @@ func main(){
 	//capacity is max num
 	fmt.Println(n1)
 	n1=append(n1,2)
-	fmt.Println(n1)
-	fmt.Println(n1)
-	fmt.Println(n1)
-	fmt.Println(n1)
-	fmt.Println(n1)
+	// fmt.Println(n1)
+	// fmt.Println(n1)
+	// fmt.Println(n1)
+	// fmt.Println(n1)
+	// fmt.Println(n1)
 	fmt.Println(cap(n1),len(n1))
 
 
 
 	//copy from one element to other element
 	var n2=make([]int,len(n1))
+	copy(n2,nums)
 	fmt.Println(len(n2),len(n1))
 	//now here is the second way for silicing
 	fmt.Println("silicing")
+	fmt.Println(len(n2))
+
+
+	//more functions
+
+	// 1) slice operator
+	var n3=[]int{1,2,3}
+	fmt.Println(n3[0:2]) 
 }
