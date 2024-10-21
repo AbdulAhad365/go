271068aec49f2242e43a75ddaa58a9323c7e3936
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-21 22:33:25 +0600

    range

---
+++ range.go
@@ -0,0 +1,22 @@
+package main
+
+//iterating over the data structures
+import "fmt"
+
+func main() {
+	//array
+	nums := []int{1, 2, 3, 4, 5}
+
+	//		WAY 1
+	for i := 0; i < len(nums); i++ {
+		fmt.Print(nums[i])
+	}
+	fmt.Print("   ")
+
+	//		WAY 2
+	for i, n1 := range nums {
+		//i=index, n1=number
+		fmt.Print(i, n1)
+		fmt.Print("----")
+	}
+}
--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -1,45 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"maps"
-)
+//iterating over the data structures
+import "fmt"
 
 func main() {
-	//maps -> hash,object, dictionary
-
-	//1) create the map
-	m := make(map[string]string)
-	n := make(map[string]int)
-	// type [string], and its value it int
-	//setting the element
-	m["name"] = "golang"
-	m["area"] = "backend"
-
-	n["val"] = 4
-	//key value not exit return the zero value
-	//for string --> "" ,int-->0, boolean-->false
-	fmt.Println(m["name"], m["area"], m["w"], n["val"], n["m"])
-
-	//len
-	fmt.Print(len(m))
-
-	//delete for deleting
-	delete(m, "area")
-	fmt.Print(m)
-
-	//to empty everything use clear
-	// clear(m)c
-
-	//			WAY 2:
-	m1 := map[string]int{"price": 24, "phones": 34}
-	m2 := map[string]int{"price": 24, "phones": 34}
-	v, ok := m1["price"]
-
-	fmt.Print("--------------- ")
-	fmt.Print(v, ok)
-
-	// fmt.Print(m1, m1["price"])
-	//compare and & check if two maps are equal
-	fmt.Println("Map1==Map2", maps.Equal(m1, m2))
+	nums := []int{1, 2, 3, 4, 5}
+	for i := 0; i < len(nums)+1; i++ {
+		fmt.Print(i)
+	}
 }
