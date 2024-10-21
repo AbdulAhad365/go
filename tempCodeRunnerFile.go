f4fdc082243b4175f8f0439add4b5317e2019101
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-21 09:13:11 +0600

    tempcode

--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -1,6 +1,36 @@
-fmt.Println(n1)
-	// fmt.Println(n1)
-	// fmt.Println(n1)
-	// fmt.Println(n1)
-	// fmt.Println(n1)
-	// fmt.Println(cap(n1),len(n1))
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//maps -> hash,object, dictionary
+
+	//1) create the map
+	m := make(map[string]string)
+	n := make(map[string]int)
+	// type [string], and its value it int
+	//setting the element
+	m["name"] = "golang"
+	m["area"] = "backend"
+
+	n["val"] = 4
+	//key value not exit return the zero value
+	//for string --> "" ,int-->0, boolean-->false
+	fmt.Println(m["name"], m["area"], m["w"], n["val"], n["m"])
+
+	//len
+	fmt.Print(len(m))
+
+	//delete for deleting
+	delete(m, "area")
+	fmt.Print(m)
+
+	//to empty everything use clear
+	// clear(m)c
+
+	//			WAY 2:
+	m1:=map[string]int{"price":24,"phones":34}
+	fmt.Print(m1)
+}
