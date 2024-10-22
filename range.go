b0375a7bef2ffc7f4e09a1790114e6f6bf200abe
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-22 23:02:22 +0600

    range

--- range.go
+++ range.go
@@ -6,6 +6,8 @@ import "fmt"
 func main() {
 	//array
 	nums := []int{1, 2, 3, 4, 5}
+	//dictionary
+	num_dic := map[string]string{"ahad": "eed", "ahmad": "ewdfd"}
 
 	//		WAY 1
 	for i := 0; i < len(nums); i++ {
@@ -14,10 +16,21 @@ func main() {
 	fmt.Print("   ")
 
 	//		WAY 2
-	for i, n1 := range nums {
-		//i=index, n1=number
-		fmt.Print(i, n1)
-		fmt.Print("----")
-	}
+	// for i, n1 := range nums {
+	// 	//i=index, n1=number
+	// 	fmt.Print(i, n1)
+	// 	fmt.Print("----")
+	// }
 	// These are some methord to make changes in the file
+	for j, n2 := range num_dic {
+		fmt.Print(j, n2)
+		fmt.Print("-----")
+	}
+
+	//to get the unicode in the letter or strings
+	for i, c := range "ahadname" {
+		fmt.Print(i, c)
+		fmt.Print("---chr
+		")
+	}
 }
--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -1,11 +1,5 @@
-package main
-
-//iterating over the data structures
-import "fmt"
-
-func main() {
-	nums := []int{1, 2, 3, 4, 5}
-	for i := 0; i < len(nums)+1; i++ {
-		fmt.Print(i)
+for i, n1 := range nums {
+		//i=index, n1=number
+		fmt.Print(i, n1)
+		fmt.Print("----")
 	}
-}
