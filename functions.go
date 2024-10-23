ccbf7d4b07efb81b875be1ceebdaab1b313141e7
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-23 22:50:20 +0600

    functions.go

---
+++ functions.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+func getLanguages() (string, string, string, string, bool) {
+	return "golang", "javascript", "c", "typescript", true
+}
+func main() {
+	result := add(3, 5)
+	fmt.Print(result)
+	fmt.Println(getLanguages())
+}
