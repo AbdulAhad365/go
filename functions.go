31ac3be7ed952a1c0ae4747eddc80288b7f37307
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-24 21:45:19 +0600

    now update the functions

--- functions.go
+++ functions.go
@@ -8,9 +8,24 @@ func add(a int, b int) int {
 func getLanguages() (string, string, string, string, bool) {
 	return "golang", "javascript", "c", "typescript", true
 }
+func process() func(a int) int {
+	return func(a int) int {
+		return a
+	}
+}
 func main() {
 	result := add(3, 5)
 	fmt.Print(result)
+	// way 1 to get the return function from getlnsng
 	fmt.Println(getLanguages())
+	// way 2:
+	lan1, lang2, lang3, lang4, _ := getLanguages()
+	// PRINT THR LANGUAGES
+	fmt.Print(lan1, lang2, lang3, lang4)
 	//ADD OTHER FUNCTIONS CALLS
+	proces := process()
+	fmt.Print(proces(4))
+
+	//call the function	func ()  {
+
 }
--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -1,5 +1,2 @@
-for i, n1 := range nums {
-		//i=index, n1=number
-		fmt.Print(i, n1)
-		fmt.Print("----")
-	}
+
+	fmt.Print(proces(4))
