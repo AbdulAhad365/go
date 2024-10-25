f7c05ecead76003a12a9912efb8f306ea0dfcd47
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-25 20:35:11 +0600

    varadic functions

---
+++ varadia_function.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int) int {
+	// func sum(nums ...interface{}) int {
+	//we get a slice from it
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+func main() {
+	// fmt.Println(1, 2, 3, 4, "hello there ")
+	total := sum(1, 2, 3, 4)
+	fmt.Print(total)
+	fmt.Print("--")
+	//way 2 to pass the slice values directly
+	nums := []int{1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10}
+	t := sum(nums...)
+	fmt.Println(t)
+}
