4b44f078192887a40ca03387abed52846cb1b933
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-28 08:51:38 +0600

    pointers

--- pointers.go
+++ pointers.go
@@ -2,14 +2,20 @@ package main
 
 import "fmt"
 
+func after_change_num(num *int) {
+	*num = 5
+	fmt.Println("changed value is: ", *num)
+}
 func change_num(num int) {
 	num = 5
 	fmt.Println(num)
 }
 func main() {
 	num := 1
+	fmt.Println("initial value is: ", num)
 	change_num(num)
 	// num will not be changed
+	after_change_num(&num)
 	fmt.Println(num)
 	//now more code
 }
