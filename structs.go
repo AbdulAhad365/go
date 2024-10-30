8e4c460a383e546d56e1f2c03f313540e3654298
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-30 08:12:59 +0600

    change in struct file

--- structs.go
+++ structs.go
@@ -14,15 +14,41 @@ type order struct {
 	createdAt time.Time //nanosecond precision
 }
 
+// to change the status
+func (o *order) change_status(stat string) {
+	o.status = stat
+}
+
+// return function
+func (o order) getAmount() string {
+	return o.status
+
+}
+
 func main() {
 	//instance
 	// var order order=
+
+	//order 1
 	myorder := order{
 		id:     "1",
 		amount: 50.00,
 		status: "recieved",
 	}
+	//order 2
+	myorder2 := order{
+		id:     "2",
+		amount: 100.0,
+		status: "not delievered",
+	}
+	//change the stat
+	myorder.change_status("paymenet done")
 	myorder.createdAt = time.Now()
+	//get the value of status
+	fmt.Println(myorder.getAmount())
 	fmt.Println("ORDER IS ", myorder)
+
+	myorder2.createdAt = time.Now()
+	fmt.Println("order 2 is ", myorder2)
 	fmt.Println(myorder.id)
 }
--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -1,13 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func change_num(num int) {
-	num = 5
-	fmt.Print(num)
+// struct order
+// struct : to group multiple things at one place
+type order struct {
+	id        string
+	amount    float32
+	status    string
+	createdAt time.Time //nanosecond precision
 }
+
+// to change the status
+func (o *order) change_status(stat string) {
+	o.status = stat
+}
+
+// return function
+func (o *order) getAmount() string {
+	return o.status
+
+}
+
 func main() {
-	num := 1
-	change_num(num)
-	fmt.Println(num)
+	//instance
+	// var order order=
+
+	//order 1
+	myorder := order{
+		id:     "1",
+		amount: 50.00,
+		status: "recieved",
+	}
+	//order 2
+	myorder2 := order{
+		id:     "2",
+		amount: 100.0,
+		status: "not delievered",
+	}
+	//change the stat
+	myorder.change_status("paymenet done")
+	myorder.createdAt = time.Now()
+	//get the value of status
+	fmt.Println(myorder.getAmount())
+	fmt.Println("ORDER IS ", myorder)
+
+	myorder2.createdAt = time.Now()
+	fmt.Println("order 2 is ", myorder2)
+	fmt.Println(myorder.id)
 }
