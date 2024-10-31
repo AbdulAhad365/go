dcc273ec12c7196ca29a49d6ebfdbcd355997f95
Abdul ahad <115157982+AbdulAhad365@users.noreply.github.com>
2024-10-31 07:31:30 +0600

    structs in the go lang

--- structs.go
+++ structs.go
@@ -14,6 +14,16 @@ type order struct {
 	createdAt time.Time //nanosecond precision
 }
 
+func newOrder(id string, amount float32, status string) *order {
+	// initial setup
+	myorder := order{
+		id:     id,
+		amount: amount,
+		status: status,
+	}
+	return &myorder
+}
+
 // to change the status
 func (o *order) change_status(stat string) {
 	o.status = stat
@@ -26,29 +36,40 @@ func (o order) getAmount() string {
 }
 
 func main() {
+	myorder := newOrder("1", 30.50, "recieved")
+	fmt.Println(myorder)
+	fmt.Println(myorder.amount)
+
+	//using the inline struct
+	language := struct {
+		name     string
+		how_am_I bool
+	}{"ahad", true}
+	fmt.Println(language.how_am_I)
 	//instance
 	// var order order=
 
-	//order 1
-	myorder := order{
-		id:     "1",
-		amount: 50.00,
-		status: "recieved",
-	}
-	//order 2
-	myorder2 := order{
-		id:     "2",
-		amount: 100.0,
-		status: "not delievered",
-	}
-	//change the stat
-	myorder.change_status("paymenet done")
-	myorder.createdAt = time.Now()
-	//get the value of status
-	fmt.Println(myorder.getAmount())
-	fmt.Println("ORDER IS ", myorder)
+	//order 1id
+	// 	myorder := order{
+	// 		id:     "1",
+	// 		amount: 50.00,
+	// 		status: "recieved",
+	// 	}
+	// 	//order 2
+	// 	myorder2 := order{
+	// 		id:     "2",
+	// 		amount: 100.0,
+	// 		status: "not delievered",
+	// 	}
+	// 	//change the stat
+	// 	myorder.change_status("paymenet done")
+	// 	myorder.createdAt = time.Now()
+	// 	//get the value of status
+	// 	fmt.Println(myorder.getAmount())
+	// 	fmt.Println("ORDER IS ", myorder)
 
-	myorder2.createdAt = time.Now()
-	fmt.Println("order 2 is ", myorder2)
-	fmt.Println(myorder.id)
+	//		myorder2.createdAt = time.Now()
+	//		fmt.Println("order 2 is ", myorder2)
+	//		fmt.Println(myorder.id)
+	//	}
 }
